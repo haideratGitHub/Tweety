b760ddca5edaf265f9365aff399e277ccd0bc19e
Mahnoor Imam <manooimam03@gmail.com>
2020-05-01 05:28:57 +0500

    Login and Signup connectivity

--- MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/Controllers/HomeController.cs
@@ -4,19 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using MvcApplication1.Models;
 namespace MvcApplication1.Controllers
 {
     public class HomeController : Controller
     {
         //
         // GET: /Home/
-
-        public ActionResult index()
+        
+        public ActionResult Login()
         {
-            return View();
-
+            if(Session["username"]==null)
+                return View();
+            else
+                return RedirectToAction("HomePage");
         }
-        public ActionResult Index2()
+        public ActionResult SignUp()
         {
             return View();
         }
@@ -27,6 +30,62 @@ namespace MvcApplication1.Controllers
         public ActionResult Loading()
         {
             return View();
+        }
+
+        public ActionResult authenticate(String username, String password)
+        {
+            int result = CRUD.Login(username, password);
+
+            if (result == -1)
+            {
+                String data = "Something went wrong while connecting with the database.";
+                return View("Login", (object)data);
+            }
+            else if (result == 0)
+            {
+
+                String data = "Incorrect Credentials";
+                return View("Login", (object)data);
+            }
+            Session["username"] = username;
+            
+            return RedirectToAction("HomePage");
+            //return RedirectToAction("HomePage", new { username = username });
+        }
+
+        public ActionResult HomePage(String username)
+        {
+            if (Session["username"] == null)
+                return View("login");
+            else
+            {
+                //List<User> users = CRUD.view_user(username);
+                User users = CRUD.view_user(Session["username"].ToString());
+                //if (users == null)
+                //{
+                //    RedirectToAction("Login");
+                //}
+                Console.Write(users);
+                return View(users);
+            }
+        }
+
+        public ActionResult Add_User(String username, String password, String first_name, String last_name, String email, String country, String gender/*,String BirthDay, String BirthMonth, String BirthYear*/ )
+        {
+            int result = CRUD.SignUp(username, password, first_name, last_name, email, country, gender);
+            if (result == -1)
+            {
+                String data = "Something went wrong while connecting with the database.";
+                return View("Login", (object)data);
+            }
+            else if (result == 0)
+            {
+                String data = "This Username is already in use";
+                return View("SignUp", (object)data);
+            }
+            Session["username"] = username;
+            return RedirectToAction("HomePage");
+            //return RedirectToAction("HomePage", new { username = username });
         }
     }
 }
