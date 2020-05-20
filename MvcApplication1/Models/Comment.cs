652e653d7a17db90fc268e0e67172ae5940568ab
Mahnoor Imam <manooimam03@gmail.com>
2020-05-20 11:44:45 +0500

    Made classes for tweets and comments

---
+++ MvcApplication1/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Models
+{
+    public class Comment
+    {
+        public string commentID;
+        public string tweetID;
+        public string commenterID;
+        public string comment;
+        public string date;
+        public string time;
+    }
+}
